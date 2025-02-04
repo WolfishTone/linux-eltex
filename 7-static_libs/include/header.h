88789237b553a072325c1c4559a739b5ca80261a
Ase Ilva <igorion98@gmail.com>
2025-02-05 01:56:47 +0700

    task 7 done

---
+++ 7-static_libs/include/header.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int add(const int op1, const int op2);
+int sub(const int op1, const int op2);
+int mul(const int op1, const int op2);
+int div(const int op1, const int op2);
---
+++ 7-static_libs/src/add.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int
+add (const int op1, const int op2)
+{
+  return op1 + op2;
+}
